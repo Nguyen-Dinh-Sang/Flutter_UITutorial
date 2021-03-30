7079b7e1d1e77ad27b6bb042b5a5b510a6e52837
DinhSang <dinhsangdroid@gmail.com>
2021-03-30 14:27:51 +0700

    FlutterUI-DinhSang: add dashboard ui

--- lib/DashboardUI.dart
+++ lib/DashboardUI.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/DrawerMenu.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DashboardUI extends StatefulWidget {
   @override
@@ -11,6 +12,12 @@ class _DashboardUIState extends State<DashboardUI> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    var cardTextStyle = TextStyle(
+      fontFamily: 'Montserrat Regular',
+      fontSize: 14.0,
+      color: Color.fromRGBO(63, 63, 63, 1),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -36,6 +43,7 @@ class _DashboardUIState extends State<DashboardUI> {
               children: <Widget>[
                 Container(
                   height: 64,
+                  margin: EdgeInsets.only(bottom: 20),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
@@ -72,16 +80,129 @@ class _DashboardUIState extends State<DashboardUI> {
                     ],
                   ),
                 ),
-                GridView.count(
-                  mainAxisSpacing: 10,
-                  crossAxisSpacing: 10,
-                  primary: false,
-                  crossAxisCount: 2,
-                  children: <Widget>[
-                    Card(
-
-                    )
-                  ],
+                Expanded(
+                  child: GridView.count(
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
+                    primary: false,
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.network(
+                              'https://image.flaticon.com/icons/svg/1904/1904425.svg',
+                              height: 128,
+                            ),
+                            Text(
+                              'Personal Data',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.network(
+                              'https://image.flaticon.com/icons/svg/1904/1904565.svg',
+                              height: 128,
+                            ),
+                            Text(
+                              'Course Schedule',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.network(
+                              'https://image.flaticon.com/icons/svg/1904/1904527.svg',
+                              height: 128,
+                            ),
+                            Text(
+                              'Attendance Recap',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.network(
+                              'https://image.flaticon.com/icons/svg/1904/1904437.svg',
+                              height: 128,
+                            ),
+                            Text(
+                              'Study Result',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.network(
+                              'https://image.flaticon.com/icons/svg/1904/1904235.svg',
+                              height: 128,
+                            ),
+                            Text(
+                              'Course Booking',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        elevation: 4,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SvgPicture.network(
+                              'https://image.flaticon.com/icons/svg/1904/1904221.svg',
+                              height: 128,
+                            ),
+                            Text(
+                              'Course Plan',
+                              style: cardTextStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
